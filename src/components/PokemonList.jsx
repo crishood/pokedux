909a52ee801487628fb293af7c70caf099e55cb7
crishood <crishood13@gmail.com>
2022-07-17 10:03:45 -0500

    Set project and create UI

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,17 +1,17 @@
-import PokemonCard from './PokemonCard';
+import PokemonCard from "./PokemonCard";
 
 const PokemonList = ({ pokemons }) => {
   return (
-    <div className='PokemonList'>
+    <div className="PokemonList">
       {pokemons.map((pokemon) => {
-        return <PokemonCard />;
+        return <PokemonCard name={pokemon.name} key={pokemon.name} />;
       })}
     </div>
   );
 };
 
 PokemonList.defaultProps = {
-  pokemons: Array(10).fill(''),
+  pokemons: Array(10).fill(""),
 };
 
 export default PokemonList;
